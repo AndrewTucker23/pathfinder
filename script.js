052bde9c6bb720a75e657170f867bb129850cdcb
AndrewTucker23 <art.tucker23@gmail.com>
2025-08-06 19:53:51 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+  const map = document.getElementById("map");
+  map.innerHTML = "<p style='text-align:center;margin-top:20px;'>[Interactive Map Would Load Here]</p>";
+});
